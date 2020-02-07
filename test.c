915befccf65102751852df8c3f853223b7f4f326
z-jiahui <z.jiahui@foxmail.com>
2020-02-07 19:21:48 +0800

    testt

---
+++ test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main()
+ 
+{
+ 
+    printf("short=%d\n\n",sizeof(short));
+ 
+   printf("int=%d\n\n",sizeof(int));
+   
+   printf("long=%d\n\n",sizeof(long));
+   
+   printf("long long=%d\n\n",sizeof(long long));
+ 
+    printf("float=%d\n\n",sizeof(float));
+   
+   printf("double=%d\n",sizeof(double));
+ 
+}
